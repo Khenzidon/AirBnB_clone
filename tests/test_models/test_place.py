88cee35b35dbbb8ce00586ffd8b6d48b3f7dd981
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-09-06 01:04:28 -0700

    Add files via upload

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,52 +1,139 @@
 #!/usr/bin/python3
-"""Unittest module for the Place Class."""
+"""
+This module contains unittests for class Place
+"""
 
-import unittest
 from datetime import datetime
-import time
-from models.place import Place
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+import inspect
+from models import place
 from models.base_model import BaseModel
+import pep8
+import unittest
+Place = place.Place
 
 
-class TestPlace(unittest.TestCase):
+class TestUserDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Place class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.place_f = inspect.getmembers(Place, inspect.isfunction)
 
-    """Test Cases for the Place class."""
+    def test_pep8_conformance_user(self):
+        """Test that models/place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    def test_pep8_conformance_test_user(self):
+        """Test that tests/test_models/test_place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_user_module_docstring(self):
+        """Test for the amenity.py module docstring"""
+        self.assertIsNot(place.__doc__, None,
+                         "place.py needs a docstring")
+        self.assertTrue(len(place.__doc__) >= 1,
+                        "place.py needs a docstring")
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_user_class_docstring(self):
+        """Test for the Amenity class docstring"""
+        self.assertIsNot(Place.__doc__, None,
+                         "Place class needs a docstring")
+        self.assertTrue(len(Place.__doc__) >= 1,
+                        "Place class needs a docstring")
 
-    def test_8_instantiation(self):
-        """Tests instantiation of Place class."""
+    def test_user_func_docstrings(self):
+        """Test for the presence of docstrings in Amenity methods"""
+        for func in self.place_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
-        b = Place()
-        self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
-        self.assertIsInstance(b, Place)
-        self.assertTrue(issubclass(type(b), BaseModel))
 
-    def test_8_attributes(self):
-        """Tests the attributes of Place class."""
-        attributes = storage.attributes()["Place"]
-        o = Place()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+class TestAmenity(unittest.TestCase):
+    """
+    Tests the functionality of the place class
+    """
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_Inheritance(self):
+        """
+        Tests that place is a subclass of BaseModel
+        """
+        place = Place()
+        self.assertIsInstance(place, BaseModel)
+        self.assertTrue(hasattr(place, "id"))
+        self.assertTrue(hasattr(place, "created_at"))
+        self.assertTrue(hasattr(place, "updated_at"))
+
+    def test_Attributes(self):
+        """
+        Tests that the class has the correct attributes
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertEqual(place.city_id, "")
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertEqual(place.user_id, "")
+        self.assertTrue(hasattr(place, "name"))
+        self.assertEqual(place.name, "")
+        self.assertTrue(hasattr(place, "description"))
+        self.assertEqual(place.description, "")
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertEqual(place.number_rooms, 0)
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertEqual(place.max_guest, 0)
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertEqual(place.price_by_night, 0)
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertEqual(place.latitude, 0.0)
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertEqual(place.longitude, 0.0)
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertEqual(place.amenity_ids, [])
+        self.assertEqual(type(place.number_rooms), int)
+        self.assertEqual(type(place.number_bathrooms), int)
+        self.assertEqual(type(place.max_guest), int)
+        self.assertEqual(type(place.price_by_night), int)
+        self.assertEqual(type(place.latitude), float)
+        self.assertEqual(type(place.longitude), float)
+        self.assertEqual(type(place.amenity_ids), list)
+
+    def test_to_dict(self):
+        """
+        Tests that to_dict method works with the class
+        """
+        pl = Place()
+        ndict = pl.to_dict()
+        self.assertEqual(type(ndict), dict)
+        for attr in pl.__dict__:
+            self.assertTrue(attr in ndict)
+            self.assertTrue("__class__" in ndict)
+
+    def test_dict_values(self):
+        """
+        Tests that dictionary has correct values
+        """
+        isoforma = "%Y-%m-%dT%H:%M:%S.%f"
+        pl = Place()
+        new_d = pl.to_dict()
+        self.assertEqual(new_d["__class__"], "Place")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], pl.created_at.strftime(isoforma))
+        self.assertEqual(new_d["updated_at"], pl.updated_at.strftime(isoforma))
+
+    def test_str(self):
+        """
+        Tests that str method returns correct output
+        """
+        place = Place()
+        string = "[Place] ({}) {}".format(place.id, place.__dict__)
+        self.assertEqual(string, str(place))
